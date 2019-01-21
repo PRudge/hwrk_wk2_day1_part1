a5b533c0342fc6c724f499fa052e16892bcb0ea4
Pauline Rudge <paulinerudge@me.com>
2019-01-21 12:44:27 +0000

    used the attr setters and getters

--- specs/student_info_spec.rb
+++ specs/student_info_spec.rb
@@ -9,26 +9,29 @@ class TestStuden < MiniTest::Test
   end
 
   def test_student_name
-  # assert_equal("Pauline", @student_info.name)
-  assert_equal("Pauline", @student_info.get_name())
+    assert_equal("Pauline", @student_info.name())
+  # assert_equal("Pauline", @student_info.get_name())
   end
 
 
   def test_student_cohort
-  # assert_equal("E28", @student_info.name)
-  assert_equal("E28", @student_info.get_cohort())
+  assert_equal("E28", @student_info.cohort)
+  # assert_equal("E28", @student_info.get_cohort())
   end
+  #
 
   def test_new_name
-    # assert_equal("Lucy", @student_info.name)
-    assert_equal("Lucy", @student_info.set_name("Lucy"))
+    @student_info.name = "Lucy"
+    assert_equal("Lucy", @student_info.name)
+    # assert_equal("Lucy", @student_info.set_name("Lucy"))
   end
-
-
-
-
-
-
+  #
+  # def test_new_cohort
+  #   # assert_equal("E29", @student_info.cohort)
+  #   assert_equal("E29", @student_info.set_cohort("E29"))
+  # end
+  #
+  #
 
 
 end
--- student_info.rb
+++ student_info.rb
@@ -1,7 +1,8 @@
 class Student
   #
   # attr_reader :name, :cohort
-  # attr_writer
+  # attr_writer :name, :cohort
+  attr_accessor :name, :cohort
 
   # need an initialize to grab the input to the class
   def initialize(input_name, input_cohort) # initialize is a ruby
@@ -9,20 +10,20 @@ class Student
     @cohort = input_cohort
   end
 
-  def get_name()
-    return @name
-  end
-
-  def get_cohort()
-    return @cohort
-  end
-
-  def set_name(new_name)
-    @name = new_name
-  end
-
-  def set_cohort(new_cohort)
-    @name = new_cohort
-  end
+  # def get_name()
+  #   return @name
+  # end
+  #
+  # def get_cohort()
+  #   return @cohort
+  # end
+  #
+  # def set_name(new_name)
+  #   @name = new_name
+  # end
+  # #
+  # def set_cohort(new_cohort)
+  #   @name = new_cohort
+  # end
 
 end
