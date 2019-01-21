21b5bab924cde83cc9a3c1e39624fc4ec3ab4f27
Pauline Rudge <paulinerudge@me.com>
2019-01-21 12:09:18 +0000

    using getters to get student name and cohort

---
+++ specs/student_info_spec.rb
@@ -0,0 +1,22 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../student_info')
+
+class TestStuden < MiniTest::Test
+  def setup
+    # create an object
+    @student_info = Student.new("Pauline","E28")
+  end
+
+  def test_student_name
+  # assert_equal("Pauline", @student_info.name)
+  assert_equal("Pauline", @student_info.get_name())
+  end
+
+  def test_student_cohort
+  # assert_equal("E28", @student_info.name)
+  assert_equal("E28", @student_info.get_cohort())
+  end
+
+
+end
---
+++ student_info.rb
@@ -0,0 +1,21 @@
+class Student
+  #
+  # attr_reader :name, :cohort
+  # attr_writer
+
+  # need an initialize to grab the input to the class
+  def initialize(input_name, input_cohort) # initialize is a ruby
+    @name = input_name
+    @cohort = input_cohort
+  end
+
+
+  def get_name()
+    return @name
+  end
+
+  def get_cohort()
+    return @cohort
+  end
+
+end
